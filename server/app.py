b0f201e8d2dc03a36f7184713ed7f66a8510b82b
Parker Parham <parkerparham@gmail.com>
2023-05-12 15:25:08 -0400

    Adds patch route to newslettersbyid

--- server/app.py
+++ server/app.py
@@ -79,6 +79,24 @@ class NewsletterByID(Resource):
         )
 
         return response
+    
+    def patch(self,id):
+        record = Newsletter.query.filter(Newsletter.id == id).first()
+        for attr in request.form:
+            setattr(record, attr, request.form[attr])
+
+        db.session.add(record)
+        db.session.commit()
+
+        response_dict = record.to_dict()
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+
 
 api.add_resource(NewsletterByID, '/newsletters/<int:id>')
 
